936c1faf6b5b1a5d3c906aa7617a1e4067162ce7
Exyzt <andrewlyem96@gmail.com>
2025-10-15 09:45:28 +0800

    Added header format

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+    return (
+        <div className="flex justify-between px-5 py-5 xl:px-100 xl:py-20">
+            <h1 className="text-white font-quicksand text-xl md:text-2xl xl:text-4xl">Exyzt</h1>
+            <h2 className="text-white font-quicksand text-xl md:text-2xl xl:text-4xl">Just Casually Exyzting</h2>
+        </div>
+    )
+}
+
+export default Header
