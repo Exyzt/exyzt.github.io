5ecd6e0207e2e296f171d8d72fce738f00a9d4a9
Exyzt <andrewlyem96@gmail.com>
2025-10-30 01:06:31 +0800

    Updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Header from "../components/Header"
+import { Wrench } from "lucide-react"
 
 function Home() {
     return (
         <>
             <Header />
+            <div className="grid items-center justify-center pt-20">
+                <Wrench color="white" className="w-10 lg:w-20 h-10 lg:h-20 mx-auto" />
+                <h1 className="font-quicksand text-xl lg:text-3xl text-white">In Progress...</h1>
+            </div>
+            
         </>
     )
 }
